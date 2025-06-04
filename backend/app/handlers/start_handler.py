2c902daca7695bbd13395a1da8f5d2ff7b2a67e5
artparve <artparve@gmail.com>
2025-06-04 20:40:50 +0300

    Small changes

--- backend/app/handlers/start_handler.py
+++ backend/app/handlers/start_handler.py
@@ -2,7 +2,7 @@ from telegram import Update
 from telegram.ext import ContextTypes, CommandHandler, Application
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text('Привет! Я псевдо-бот YandexGPT. Отправб мне сообщение и я его перенаправлю на YandexGPT.')
+    await update.message.reply_text('Привет! Я чат-бот TOKEN. Отвечу на все интересующие вопросы.')
 
 def register_start(app: Application):
     app.add_handler(CommandHandler('start', start))
